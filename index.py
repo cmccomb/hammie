3d393a7f1f6dc817d9be3dc79ea3a1590f015510
Chris McComb <ccmcc2012@gmail.com>
2022-10-08 19:39:59 -0400

    Added error handling

--- index.py
+++ index.py
@@ -23,6 +23,12 @@ def say_hello_regex(say, context):
     say(f"{greeting}, how are you?")
 
 
+@app.error
+def custom_error_handler(error, body, logger):
+    logger.exception(f"Error: {error}")
+    logger.info(f"Request body: {body}")
+
+
 
 @app.message("flip a coin")
 def ask_who(message, say):
