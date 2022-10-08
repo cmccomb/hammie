21eff927db40ad78c5707e59e32c9902f9ca545a
Chris McComb <ccmcc2012@gmail.com>
2022-10-08 13:38:03 -0400

    Adding flip a coin

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# glitch and bolt(python) sample
+# Hammie
 
 This is a sample for slack bolt by python on glitch.
--- index.py
+++ index.py
@@ -4,10 +4,10 @@ import slack_bolt
 from os.path import join, dirname
 from dotenv import load_dotenv
 import json
+import random
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-
 
 # Initializes your app with your bot token and signing secret
 app = slack_bolt.App(
@@ -21,10 +21,38 @@ def say_hello_regex(say, context):
     greeting = context['matches'][0]
     say(f"{greeting}, how are you?")
 
-    
-@app.message("knock knock")
+
+
+@app.message("flip a coin")
 def ask_who(message, say):
-    say("_Who's there?_")
+    if random.random() < 0.5:
+        say(json.loads(
+            """
+                {
+                    "blocks": [
+                        {
+                            "type": "image",
+                            "image_url": "https://www.usmint.gov/wordpress/wp-content/uploads/2021/12/2022-american-women-quarters-coin-uncirculated-obverse-philadelphia-768x768.jpg",
+                            "alt_text": "heads"
+                        }
+                    ]
+                }
+            """
+            ))
+    else:
+        say(json.loads(
+            """
+                {
+                    "blocks": [
+                        {
+                            "type": "image",
+                            "image_url": " https://www.usmint.gov/wordpress/wp-content/uploads/2021/12/2022-american-women-quarters-coin-sally-ride-uncirculated-reverse-768x768.jpg",
+                            "alt_text": "heads"
+                        }
+                    ]
+                }
+            """
+            ))
     
     
 @app.message(re.compile("^debug_regex"))
