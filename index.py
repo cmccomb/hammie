bacf61d4c0b0a46046aeab0d935e16931b5c3ea2
Chris McComb <ccmcc2012@gmail.com>
2022-10-08 20:10:13 -0400

    Say hello with name

--- index.py
+++ index.py
@@ -9,20 +9,20 @@ import random
 # Set up dotenv
 dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
-# Initializes your app with your bot token and signing secret
+# Initializes the app with the bot token and signing secret
 app = slack_bolt.App(
     token=os.environ.get("SLACK_BOT_TOKEN"),
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
-
+# Basic greeting
 @app.message(re.compile("(hi|hello|hey)"))
 def say_hello_regex(say, context):
     greeting = context['matches'][0]
     print(context)
-    say(f"{greeting}, how are you?")
+    say(f"{greeting} <{context['user_id']}>, how are you?")
 
-
+# Flip a coin and show result as image
 @app.message("flip a coin")
 def ask_who(message, say):
     if random.random() < 0.5:
@@ -53,28 +53,30 @@ def ask_who(message, say):
                 }
             """
             ))
-    
-    
+
+
+# Show structure of a regex context
 @app.message(re.compile("^(debug_regex)"))
 def debug_regex(say, context):
     jstring = json.dumps(context, default=lambda x: "[[ Cannot be serialized ]]", indent="\t")
     say(f"```{jstring}```")
   
-    
+
+# Show structure of a string message
 @app.message("debug_string")
 def debug_string(message, say):
     jstring = json.dumps(message, indent="\t")
     say(f"```{jstring}```")
 
 
+# Catch all at the end and admit that it don't make no sense
 @app.message(re.compile(".*"))
 def last_resort(say, context):
     message = context['matches'][0]
     print(context)
     say(f"Sorry, but I have no idea what you mean by \"{message}\". Can you try to ask it in a different way?")
 
-
     
-# Start your app
+# Start the app
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
