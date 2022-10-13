f61b79593f433da8816d943e313a6c26fc625d1d
Chris McComb <ccmcc2012@gmail.com>
2022-10-13 14:14:23 -0400

    This should break coinflip

--- index.py
+++ index.py
@@ -18,46 +18,6 @@ app = slack_bolt.App(
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
-# Matches for checking
-is_greeting = re.compile("(hi|hello|hey|yo)", re.IGNORECASE)
-is_anything = re.compile(".*")
-is_coinflip = re.compile("^(flip|coin|quarter)$")
-
-# Textblock
-def text_block(markdown_string):
-    return {
-        "type": "section",
-        "text": {
-            "type": "mrkdwn",
-            "text": markdown_string
-        }
-    }
-
-# Quarters
-quarter_heads = """
-                {
-                    "blocks": [
-                        {
-                            "type": "image",
-                            "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/2006_Quarter_Proof.png/780px-2006_Quarter_Proof.png",
-                            "alt_text": "heads"
-                        }
-                    ]
-                }
-            """
-
-quarter_tails = """
-                {
-                    "blocks": [
-                        {
-                            "type": "image",
-                            "image_url": "https://upload.wikimedia.org/wikipedia/commons/6/6f/1999_PA_Proof.png",
-                            "alt_text": "tails"
-                        }
-                    ]
-                }
-            """
-
 # Basic greeting
 @app.message(is_greeting)
 def greetings(say, context):
