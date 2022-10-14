0d97c12d498837d6d858bd0c7849f90f468affea
Chris McComb <ccmcc2012@gmail.com>
2022-10-14 18:42:54 -0400

    Better acronyms

--- index.py
+++ index.py
@@ -43,7 +43,7 @@ help_list.append(greetings.__doc__)
 )
 @app.message(is_acronym)
 def acronym_search(say, context):
-    """👋 `What does ASME stand for?`: I can help you learn common acronyms in our field."""
+    """What does ASME stand for?: I can help you learn common acronyms in our field."""
     acronym = is_acronym.search(context['matches'](0)).group(2)
     if acronym in ACRONYMS:
         say(f"{acronym}: {ACRONYMS[acronym]}")
