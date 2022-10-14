73884875ddd00058797b719330c5b6f89c1e1474
Chris McComb <ccmcc2012@gmail.com>
2022-10-14 18:46:57 -0400

    Better acronym match

--- index.py
+++ index.py
@@ -60,7 +60,7 @@ help_list.append(acronym_search.__doc__)
 )
 @app.message(is_branding)
 def branding(say, context):
-    """🤓 `brand`, `logo`, `font`: I'll return useful links for the lab brand."""
+    """`brand`, `logo`, `font`: I'll return useful links for the lab brand."""
     raw_json = {
         "blocks": [
             {
